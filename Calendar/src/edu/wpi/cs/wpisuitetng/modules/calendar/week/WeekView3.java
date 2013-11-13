1804e5d8a7147f12cfb1eb4305866090be6bd455
magwitch324 <magwithc324@gmail.com>
2013-11-12 20:23:33 -0500

    added date calculation

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/week/WeekView3.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/week/WeekView3.java
@@ -5,11 +5,15 @@ import javax.swing.JLabel;
 import javax.swing.JTable;
 
 import java.awt.Component;
+
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
 import java.awt.Color;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class WeekView3 extends JPanel {
 	private JTable table;
@@ -19,7 +23,28 @@ public class WeekView3 extends JPanel {
 	 */
 	public WeekView3() {
 		setLayout(null);
-		JLabel lblMonthdayyear = new JLabel("Month/Day/Year");
+		
+		GregorianCalendar start = startDate();
+        String weekName = new SimpleDateFormat("MMM").format(start.getTime()) + " " + start.get(Calendar.DAY_OF_MONTH);
+        String sunday = start.get(Calendar.DAY_OF_MONTH) + " Sunday";
+        start.add(Calendar.DAY_OF_YEAR, 1);
+        String monday = start.get(Calendar.DAY_OF_MONTH) + " Monday";
+        start.add(Calendar.DAY_OF_YEAR, 1);
+        String tuesday = start.get(Calendar.DAY_OF_MONTH) + " Tuesday";
+        start.add(Calendar.DAY_OF_YEAR, 1);
+        String wednesday = start.get(Calendar.DAY_OF_MONTH) + " Wednesday";
+        start.add(Calendar.DAY_OF_YEAR, 1);
+        String thursday = start.get(Calendar.DAY_OF_MONTH) + " Thursday";
+        start.add(Calendar.DAY_OF_YEAR, 1);
+        String friday = start.get(Calendar.DAY_OF_MONTH) + " Friday";
+        start.add(Calendar.DAY_OF_YEAR, 1);
+        String saturday = start.get(Calendar.DAY_OF_MONTH) + " Saturday";
+        weekName += " - " + new SimpleDateFormat("MMM").format(start.getTime()) + " " + start.get(Calendar.DAY_OF_MONTH);
+                                        
+        
+		
+		
+		JLabel lblMonthdayyear = new JLabel(weekName);
 		lblMonthdayyear.setBounds(183, 5, 79, 14);
 		add(lblMonthdayyear);
 		
@@ -28,7 +53,7 @@ public class WeekView3 extends JPanel {
 		scrollPane.setBounds(10, 30, 430, 259);
 		add(scrollPane);
 		
-		String[] columns = {"Time", "Sun", " Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
+		String[] columns = {"Time", sunday, monday, tuesday, wednesday, thursday, friday, saturday};
 		String[][] data = {{"12:00AM", "", "", "", "", "", "", ""},
 				           {"", "", "", "", "", "", "", ""},
 		                   {"1:00AM", "", "", "", "", "", "", ""},
@@ -83,4 +108,10 @@ public class WeekView3 extends JPanel {
 		table = new JTable(model);
 		scrollPane.setViewportView(table);
 	}
+    public GregorianCalendar startDate(){
+        GregorianCalendar tmp = new GregorianCalendar();
+        tmp.add(Calendar.DAY_OF_WEEK, 
+              tmp.getFirstDayOfWeek() - tmp.get(Calendar.DAY_OF_WEEK));
+        return tmp;
+}
 }
