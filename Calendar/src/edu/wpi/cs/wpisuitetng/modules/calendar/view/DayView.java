11d22c538eee2e9be27bb0707c2f88b52a71ce16
Tucker Tibbetts <cttibbetts@wpi.edu>
2013-11-16 13:33:19 -0500

    Added Label creation to DayView

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/DayView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/DayView.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs.wpisuitetng.modules.calendar.view;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 public class DayView extends CalendarView {
 
@@ -16,6 +17,11 @@ public class DayView extends CalendarView {
 	@Override
 	public void setRange(Calendar calendar) {
 		day = calendar;
+		String dayName = day.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH);
+		String dayNum = day.getDisplayName(Calendar.DAY_OF_MONTH, Calendar.LONG, Locale.ENGLISH);
+		String monthName = day.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
+		String year = day.getDisplayName(Calendar.YEAR, Calendar.LONG, Locale.ENGLISH);
+		setLabel(dayName + ", " + monthName + " " + dayNum + ", " + year);
 		refresh();
 	}
 
