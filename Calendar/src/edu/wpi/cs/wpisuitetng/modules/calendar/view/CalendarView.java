8565e8c9d7700347110afb4beb3fc9b44918094a
Tucker Tibbetts <cttibbetts@wpi.edu>
2013-11-15 20:52:17 -0500

    Finished CalendarView abstract class

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CalendarView.java
@@ -3,10 +3,89 @@
  */
 package edu.wpi.cs.wpisuitetng.modules.calendar.view;
 
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+import java.util.Calendar;
+
 /**
  * @author cttibbetts
  *
  */
-public abstract class CalendarView {
+public abstract class CalendarView extends JSplitPane {
+	
+	private ICalPane calPane;
+	private CommitmentView commitments;
+	private String dateRange;
+	
+	
+	/**
+	 * Constructor
+	 * Sets up the panel with the refresh function
+	 */
+	public CalendarView(Calendar calendar) {
+		this.refresh();
+	}	
+	
+	/**
+	 * create and display View componenets
+	 */
+	public void refresh() {
+		
+		setLeftComponent(calPane.getPane());
+		setRightComponent(makeRightView());
+		
+	}
+	
+	/**
+	 * @return
+	 */
+	private JPanel makeRightView() {
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JPanel labelPanel = new JPanel();
 
+		JLabel dateLabel = new JLabel(dateRange);
+        dateLabel.setAlignmentX(CENTER_ALIGNMENT);
+        dateLabel.setAlignmentY(CENTER_ALIGNMENT);
+		
+		labelPanel.add(dateLabel);
+		
+		panel.add(labelPanel);
+		
+		panel.add(commitments);
+		
+		return panel;
+		
+	}
+	
+	/**
+	 * set the label for the current date/dates
+	 * @param range
+	 */
+	protected void setLabel(String range) {
+		dateRange = range;
+	}
+	
+	/**
+	 * set the new date range for the calendar
+	 * @param calendar
+	 */
+	abstract public void setRange(Calendar calendar);
+	
+	/**
+	 * Move the range forward one unit in time
+	 */
+	abstract public void nextUnit();
+	/**
+	 * Move the range backward one unit in time
+	 */
+	abstract public void prevUnit();
+	
+	
+	
 }
---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CommitmentView.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.calendar.view;
+
+import javax.swing.JPanel;
+
+/**
+ * @author cttibbetts
+ *
+ */
+public class CommitmentView extends JPanel {
+
+	
+	
+}
