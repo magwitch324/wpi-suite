fd9cc54e0a46bbebaa983a068bf5c0cc1dcd4b67
Andrew Roskuski <aroskusi@gmail.com>
2013-11-22 20:42:29 -0500

    Completed commitments are hidden from lists in the GUI

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CommitmentView.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/CommitmentView.java
@@ -108,69 +108,71 @@ public class CommitmentView extends JPanel {
 		commitPanel.setLayout(commPanelLayout);
 		List<CommitmentViewPanel> commPanelList = new ArrayList<CommitmentViewPanel>();
 		for(int i = 0; i < commitmentList.size(); i++){
-			//Commitment commit = new Commitment();
-			// commit.setName("Commitment Name");
-			// commit.setDueDate(new Date());
-			// commit.setDescription("The description of this commitment is right here. This will be shown as the description.");
-			CommitmentViewPanel commitmentPanel = new CommitmentViewPanel(commitmentList.get(i));
-			commitmentPanel.setBackground(Color.LIGHT_GRAY);
-			//commitmentPanel.setBorder((BorderFactory.createMatteBorder(
-			//        -2, -2, -2, -2, Color.GRAY)));
-			JLabel name = new JLabel("Name: "+commitmentList.get(i).getName());
-			JLabel date = new JLabel("Due Date: "+ commitmentList.get(i).getDueDate());
-			JLabel description = new JLabel("<HTML>Description: "+ commitmentList.get(i).getDescription()+"</HTML>");
-			JLabel status = new JLabel("Status: " + Status.convertToString(commitmentList.get(i).getStatus().id));
-			commitmentPanel.setLayout(new GridBagLayout());
-			GridBagConstraints c = new GridBagConstraints();
-			c.anchor = GridBagConstraints.LINE_START;
-			c.fill = GridBagConstraints.HORIZONTAL;
-			c.weightx = 1;
-			c.gridx = i;
-			commitmentPanel.add(name,c);
-			commitmentPanel.add(date,c);
-			commitmentPanel.add(description,c);
-			commitmentPanel.add(status,c);
-			//  description.setMaximumSize(new Dimension(285,300));
-			commitmentPanel.setBorder(new EmptyBorder(10, 5, 10 , 20));
-			// commitmentPanel.setPreferredSize(new Dimension(280,300));
-			// commitmentPanel.setMinimumSize(new Dimension(290, 400));
-			// commitmentPanel.setMaximumSize(new Dimension(3000,1000));
-			//  SpringLayout layoutDescription = new SpringLayout();
-			//   description.setLayout(layoutDescription);
-			// layoutDescription.putConstraint(SpringLayout.WEST, description, 0, SpringLayout.WEST, commitmentPanel);
-			//  layoutDescription.putConstraint(SpringLayout.EAST, description, 0, SpringLayout.EAST, commitmentPanel);
-			//  layoutDescription.putConstraint(SpringLayout.NORTH, description, 0, SpringLayout.NORTH, commitmentPanel);
-			//   layoutDescription.putConstraint(SpringLayout.SOUTH, description, 0, SpringLayout.SOUTH, commitmentPanel);
-			//	        commitmentPanelList.add(i,commitmentPanel);
-			commitmentPanel.addMouseListener(new MouseAdapter() {
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					if (e.getClickCount() > 1)
-						GUIEventController.getInstance().editCommitment(((CommitmentViewPanel)e.getComponent()).getCommitment(), tcalendar.getCalData());
-				}		
-			});
+			if (commitmentList.get(i).getStatus().id != 2) {
+				//Commitment commit = new Commitment();
+				// commit.setName("Commitment Name");
+				// commit.setDueDate(new Date());
+				// commit.setDescription("The description of this commitment is right here. This will be shown as the description.");
+				CommitmentViewPanel commitmentPanel = new CommitmentViewPanel(commitmentList.get(i));
+				commitmentPanel.setBackground(Color.LIGHT_GRAY);
+				//commitmentPanel.setBorder((BorderFactory.createMatteBorder(
+				//        -2, -2, -2, -2, Color.GRAY)));
+				JLabel name = new JLabel("Name: "+commitmentList.get(i).getName());
+				JLabel date = new JLabel("Due Date: "+ commitmentList.get(i).getDueDate());
+				JLabel description = new JLabel("<HTML>Description: "+ commitmentList.get(i).getDescription()+"</HTML>");
+				JLabel status = new JLabel("Status: " + Status.convertToString(commitmentList.get(i).getStatus().id));
+				commitmentPanel.setLayout(new GridBagLayout());
+				GridBagConstraints c = new GridBagConstraints();
+				c.anchor = GridBagConstraints.LINE_START;
+				c.fill = GridBagConstraints.HORIZONTAL;
+				c.weightx = 1;
+				c.gridx = i;
+				commitmentPanel.add(name,c);
+				commitmentPanel.add(date,c);
+				commitmentPanel.add(description,c);
+				commitmentPanel.add(status,c);
+				//  description.setMaximumSize(new Dimension(285,300));
+				commitmentPanel.setBorder(new EmptyBorder(10, 5, 10 , 20));
+				// commitmentPanel.setPreferredSize(new Dimension(280,300));
+				// commitmentPanel.setMinimumSize(new Dimension(290, 400));
+				// commitmentPanel.setMaximumSize(new Dimension(3000,1000));
+				//  SpringLayout layoutDescription = new SpringLayout();
+				//   description.setLayout(layoutDescription);
+				// layoutDescription.putConstraint(SpringLayout.WEST, description, 0, SpringLayout.WEST, commitmentPanel);
+				//  layoutDescription.putConstraint(SpringLayout.EAST, description, 0, SpringLayout.EAST, commitmentPanel);
+				//  layoutDescription.putConstraint(SpringLayout.NORTH, description, 0, SpringLayout.NORTH, commitmentPanel);
+				//   layoutDescription.putConstraint(SpringLayout.SOUTH, description, 0, SpringLayout.SOUTH, commitmentPanel);
+				//	        commitmentPanelList.add(i,commitmentPanel);
+				commitmentPanel.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						if (e.getClickCount() > 1)
+							GUIEventController.getInstance().editCommitment(((CommitmentViewPanel)e.getComponent()).getCommitment(), tcalendar.getCalData());
+					}		
+				});
 
-			commPanelList.add(i, commitmentPanel);
-			commitmentPanel.setMaximumSize(new Dimension(2000,100));
-			if(i > 0)
-				commPanelLayout.putConstraint(SpringLayout.NORTH, commitmentPanel, 1, SpringLayout.SOUTH, commPanelList.get(i-1));
-			else
-				commPanelLayout.putConstraint(SpringLayout.NORTH, commitmentPanel, 0, SpringLayout.NORTH, commitPanel);
+				commPanelList.add(i, commitmentPanel);
+				commitmentPanel.setMaximumSize(new Dimension(2000,100));
+				if(i > 0)
+					commPanelLayout.putConstraint(SpringLayout.NORTH, commitmentPanel, 1, SpringLayout.SOUTH, commPanelList.get(i-1));
+				else
+					commPanelLayout.putConstraint(SpringLayout.NORTH, commitmentPanel, 0, SpringLayout.NORTH, commitPanel);
 
-			commPanelLayout.putConstraint(SpringLayout.WEST, commitmentPanel, 0, SpringLayout.WEST, commitPanel);
-			commPanelLayout.putConstraint(SpringLayout.EAST, commitmentPanel, 0, SpringLayout.EAST, commitPanel);
+				commPanelLayout.putConstraint(SpringLayout.WEST, commitmentPanel, 0, SpringLayout.WEST, commitPanel);
+				commPanelLayout.putConstraint(SpringLayout.EAST, commitmentPanel, 0, SpringLayout.EAST, commitPanel);
 
-			commitPanel.add(commitmentPanel);
-			JSeparator separator = new JSeparator();
-			separator.setOrientation(JSplitPane.VERTICAL_SPLIT);
-			commPanelLayout.putConstraint(SpringLayout.NORTH, separator, 1, SpringLayout.SOUTH, commitmentPanel);
-			commPanelLayout.putConstraint(SpringLayout.WEST, separator, 0, SpringLayout.WEST, commitPanel);
-			commPanelLayout.putConstraint(SpringLayout.EAST, separator, 0, SpringLayout.EAST, commitPanel);
+				commitPanel.add(commitmentPanel);
+				JSeparator separator = new JSeparator();
+				separator.setOrientation(JSplitPane.VERTICAL_SPLIT);
+				commPanelLayout.putConstraint(SpringLayout.NORTH, separator, 1, SpringLayout.SOUTH, commitmentPanel);
+				commPanelLayout.putConstraint(SpringLayout.WEST, separator, 0, SpringLayout.WEST, commitPanel);
+				commPanelLayout.putConstraint(SpringLayout.EAST, separator, 0, SpringLayout.EAST, commitPanel);
 
-			commitPanel.add(separator);
-			if (i == commitmentList.size() - 1)
-				commPanelLayout.putConstraint(SpringLayout.SOUTH, commitPanel, 0, SpringLayout.SOUTH, separator);
+				commitPanel.add(separator);
+				if (i == commitmentList.size() - 1)
+					commPanelLayout.putConstraint(SpringLayout.SOUTH, commitPanel, 0, SpringLayout.SOUTH, separator);
 
+			}
 		}
 
 		revalidate();
