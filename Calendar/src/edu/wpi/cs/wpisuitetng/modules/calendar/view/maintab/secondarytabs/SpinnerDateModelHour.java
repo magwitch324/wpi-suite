7ef941ffaa8b74000403a1ecfdc845c4aebb9a56
piaoasd123 <piaoasd123@163.com>
2013-12-05 18:44:41 -0500

    Added 2 sources files to support new time spinner

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/maintab/secondarytabs/SpinnerDateModelHour.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2013 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: CS Anonymous
+ ******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.calendar.view.maintab.secondarytabs;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.swing.SpinnerDateModel;
+
+/**
+ * @author sfp
+ * SpinnerDateModel Increments time value by 30 minutes
+ */
+public class SpinnerDateModelHour extends SpinnerDateModel {
+
+
+	public SpinnerDateModelHour() {
+		// TODO Auto-generated constructor stub
+		super(new Date(0), null, null, Calendar.HOUR);
+	}
+
+	public SpinnerDateModelHour(Date value, Comparable start,
+			Comparable end, int calendarField) {
+		super(value, start, end, calendarField);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Object getNextValue()  
+	   {  
+	      Calendar cal = new GregorianCalendar();
+	      cal.setTime((Date)super.getValue());
+	      cal.add(Calendar.HOUR_OF_DAY, 1);  
+	      super.setValue(cal.getTime());
+	      return super.getValue();  
+	   }  
+	   public Object getPreviousValue()  
+	   {  
+		  Calendar cal = new GregorianCalendar();
+	      cal.setTime((Date)super.getValue());
+	      cal.add(Calendar.HOUR_OF_DAY, -1);  
+	      super.setValue(cal.getTime());
+	      return super.getValue();// substract 15 minutes  
+	   }  
+	   public Object getValue()
+	   {
+		   return super.getValue();
+	   }
+	   public void setValue(Date newTime)
+	   {
+		   if ((newTime == null) || !(newTime instanceof Date)) {
+	            throw new IllegalArgumentException("illegal value");
+	        }
+		   if (!newTime.equals(super.getValue())) {
+			   super.setValue(newTime);
+	           fireStateChanged();
+		   }
+	   }
+
+}
