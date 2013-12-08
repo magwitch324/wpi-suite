af25eb94766f55b9c1f74df3e61124ced2aa0088
TeresaZhao <tzhao@wpi.edu>
2013-12-08 15:33:32 -0500

    added testing for getCategories

--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/datatypes/CategoryListTest.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/datatypes/CategoryListTest.java
@@ -35,20 +35,28 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.datatypes.Status;
 public class CategoryListTest {
 
 	private  CategoryList categoryList;
-	private List<Category> categories;
-	private Category lastYear = new Category(0, "Last Year");
-	private Category lastMonth = new Category(1, "Last Month");
-	private Category lastWeek = new Category(2, "Last Week");
-	private Category todayCategory = new Category(3, "Today");
-	private Category nextWeek = new Category(4, "Next Week");
-	private Category nextMonth = new Category(5, "Next Month");
-	private Category nextYear = new Category(6, "Next Year");
-	
-	@Before
-	public void setUp() {
-	    	categoryList = new CategoryList();
-	    	categories = new ArrayList<Category>();
-	    }
+    private List<Category> categories;
+    private Category lastYear;
+    private Category lastMonth;
+    private Category lastWeek;
+    private Category todayCategory;
+    private Category nextWeek;
+    private Category nextMonth;
+    private Category nextYear;
+    
+    @Before
+    public void setUp() {
+    	categoryList = new CategoryList();
+    	categories = new ArrayList<Category>();
+        lastYear = new Category(0, "Last Year");
+        lastMonth = new Category(1, "Last Month");
+        lastWeek = new Category(2, "Last Week");
+        todayCategory = new Category(3, "Today");
+        nextWeek = new Category(4, "Next Week");
+        nextMonth = new Category(5, "Next Month");
+        nextYear = new Category(6, "Next Year");
+    }
+
 
 	/**
 	 * Test to ensure addCategory works correctly
@@ -70,8 +78,7 @@ public class CategoryListTest {
 	    categoryList.addCategory(nextWeek);
 	    categoryList.addCategory(todayCategory);
 	    categoryList.addCategory(lastYear);
-	    categoryList.addCategory(nextYear);
-	    	
+	    categoryList.addCategory(nextYear);    	
 	    assertEquals("Last Year", categoryList.getCategory(0).getName());
 	    assertEquals("Next Year", categoryList.getCategory(6).getName());
 	    assertEquals("Today", categoryList.getCategory(3).getName());
@@ -85,8 +92,7 @@ public class CategoryListTest {
 	    	categoryList.addCategory(nextWeek);
 	    	categoryList.addCategory(todayCategory);
 	    	categoryList.addCategory(lastYear);
-	    	categoryList.addCategory(nextYear);
-	    	
+	    	categoryList.addCategory(nextYear);    	
 	    	categoryList.removeCategory(0);
 	    	categoryList.removeCategory(3);
 	    	assertEquals(2, categoryList.getSize());
@@ -106,21 +112,32 @@ public class CategoryListTest {
 	    	categoryList.removeAll();
 	    	assertEquals(0, categoryList.getSize());
 	    }
+	 
 	 /**
 	  * Test to ensure adds the given array of categories work correctly
 	  */
-	 
 	 @Test
 	    public void addCategoriesTest() {
 	    	Category[] categoryArray = new Category[]{lastYear, todayCategory, nextYear};
 	    	categoryList.addCategorys(categoryArray);
-	    	
 	    	assertEquals(3, categoryList.getSize());
 	    	assertEquals("Last Year", categoryList.getElementAt(2).getName());
 	    	assertEquals("Today", categoryList.getElementAt(0).getName());
 	    }
-
-
-
+	 
+	 /**
+	  * Test to ensure getCategorys works correctly
+	  */
+	 @Test
+	    public void getCategoriesTest() {
+	    	categoryList.addCategory(nextWeek);
+	    	categoryList.addCategory(todayCategory);
+	    	categoryList.addCategory(lastYear);
+	    	categoryList.addCategory(nextYear);	    	
+	    	categories = categoryList.getCategorys();	    	
+	    	assertEquals(4, categories.size());
+	    	assertEquals("Next Year", categories.get(2).getName());
+	    	assertEquals("Last Year", categories.get(0).getName());
+	    }
 
 }
