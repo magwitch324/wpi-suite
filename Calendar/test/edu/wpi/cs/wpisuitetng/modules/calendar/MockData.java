77b3236a2c764c3ec5cd1bea1de33975c89b0073
TeresaZhao <tzhao@wpi.edu>
2013-12-10 19:34:51 -0500

    changed copy rights to for testing

--- Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/MockData.java
+++ Calendar/test/edu/wpi/cs/wpisuitetng/modules/calendar/MockData.java
@@ -6,8 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *    Andrew Hurle
+ * Contributors: CS Anonymous
  ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.calendar;
 
