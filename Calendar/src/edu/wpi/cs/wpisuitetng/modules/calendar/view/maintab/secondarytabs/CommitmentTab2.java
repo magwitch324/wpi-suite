6d1ae00412c520a5d30041b00fa9cc31fc2712a9
Tucker Tibbetts <cttibbetts@wpi.edu>
2013-12-12 21:10:43 -0500

    Forgot to add file

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/maintab/secondarytabs/CommitmentTab2.java
@@ -0,0 +1,106 @@
+package edu.wpi.cs.wpisuitetng.modules.calendar.view.maintab.secondarytabs;
+
+import static javax.swing.SpringLayout.EAST;
+import static javax.swing.SpringLayout.NORTH;
+import static javax.swing.SpringLayout.WEST;
+import static javax.swing.SpringLayout.SOUTH;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class CommitmentTab2 extends JPanel {
+
+	/**
+	 * Constructor for the Add Commitment Tab
+	 * This tab has the fields for:
+	 * Name
+	 * Description
+	 * Category
+	 * Type (personal/team)
+	 * Due Time
+	 * Due Date
+	 * Status (new/in progress/closed)
+	 * @param openedFrom
+	 */
+	public CommitmentTab2(int openedFrom) {
+		SpringLayout mainLayout = new SpringLayout();
+		setLayout(mainLayout);
+		
+		// Populate all the fields
+		JPanel nameField = getNameField();
+		JPanel descField = getDescField();
+		JPanel categoryField = getCategoryField();
+		JPanel typeField = getTypeField();
+		JPanel dueField = getDueField();
+		JPanel statusField = getStatusField();
+		
+		nameField.setBackground(Color.RED);
+		
+		add(nameField);
+		mainLayout.putConstraint(WEST, nameField, 5, WEST, this);
+		mainLayout.putConstraint(NORTH, nameField, 5, NORTH, this);
+		mainLayout.putConstraint(EAST, nameField, -5, EAST, this);
+		mainLayout.putConstraint(SOUTH, nameField, -5, SOUTH, this);
+		
+	}
+	
+	//TODO Add a constructor for editing a commitment tab -- CommitmentTab line 200
+	
+	private JPanel getNameField() {
+		JPanel nameField = new JPanel();
+		SpringLayout layout = new SpringLayout();
+		nameField.setLayout(layout);
+		
+		// Create and add Label to the nameField
+		JLabel label = new JLabel("Name: ");
+		nameField.add(label);
+		layout.putConstraint(WEST, label, 5, WEST, nameField);
+		layout.putConstraint(NORTH, label, 5, NORTH, nameField);
+		layout.putConstraint(SOUTH, label, -5, SOUTH, nameField);
+		
+		// Create and add textField to the nameField
+		JTextField textField = new JTextField();
+		nameField.add(textField);
+		layout.putConstraint(WEST, textField, 5, EAST, label);
+		layout.putConstraint(NORTH, textField, 5, NORTH, nameField);
+		layout.putConstraint(EAST, textField, -5, EAST, nameField);
+		layout.putConstraint(SOUTH, textField, -5, SOUTH, nameField);
+		
+		return nameField;
+	}
+	
+	private JPanel getDescField() {
+		//TODO
+		return null;
+	}
+	
+	private JPanel getCategoryField() {
+		//TODO
+		return null;
+		
+	}
+	
+	private JPanel getTypeField() {
+		//TODO
+		return null;
+		
+	}
+	
+	private JPanel getDueField() {
+		//TODO
+		return null;
+		
+	}
+	
+	private JPanel getStatusField() {
+		//TODO
+		return null;
+		
+	}
+	
+}
