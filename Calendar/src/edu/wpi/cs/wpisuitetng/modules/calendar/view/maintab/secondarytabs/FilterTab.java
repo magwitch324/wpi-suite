c6310a4b7ebb4cf0e2eb0118a86b5a4c068496ba
Seiichiro Ueda <sueda@wpi.edu>
2013-12-14 14:43:51 -0500

    Clicking new filter displays the editing pane

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/maintab/secondarytabs/FilterTab.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/maintab/secondarytabs/FilterTab.java
@@ -14,11 +14,14 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -48,10 +51,10 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.CalendarStandard;
 	private JPanel buttonPanel;
 	private JButton btnAddFilter;
 	private AbstractButton btnCancel;
-	private final Container viewPanel;
+	private Container viewPanel;
 	private JButton btnDelete;
-	private Component btnEdit;
-	private Component btnNewFilter;
+	private JButton btnEdit;
+	private JButton btnNewFilter;
 	private boolean initFlag;
 	private JScrollPane scrollPane;
 	private Component categoryList;
@@ -95,6 +98,19 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.CalendarStandard;
 		gridBagLayout.rowWeights = new double[]{0.0};
 		setLayout(gridBagLayout);
 
+			
+		
+		addFilterList();
+		addListeners();
+		//editFilterMode();
+		initFlag = true;
+		}
+	
+	/**
+	 * Method FilterList.
+	 */
+	public void addFilterList(){
+		
 		//final JPanel aPanel = new JPanel();
 		viewPanel = new JPanel();
 		viewPanel.setBackground(Color.WHITE);
@@ -107,27 +123,14 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.CalendarStandard;
 		gbl.rowHeights = new int[] {0, 0, 0};
 		gbl.columnWidths = new int[] {0};
 		viewPanel.setLayout(gbl);
-		
+				
 		final GridBagConstraints constraints = new GridBagConstraints();
 		constraints.weightx = 1;
 		constraints.gridx = 0;
 		constraints.weighty = 1;
 		constraints.fill = GridBagConstraints.BOTH;
-		add(viewPanel, constraints);		
+		add(viewPanel, constraints);	
 		
-		/*addLabels();
-		addEditableElements();
-		setDefaultValuesForEditableElements();
-		addEditableElementsListeners();*/
-		addFilterList();
-		editingFilterMode();
-		initFlag = true;
-		}
-	
-	/**
-	 * Method FilterList.
-	 */
-	public void addFilterList(){
 		scrollPane = new JScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, 
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scrollPane.getViewport().setBackground(Color.WHITE);
@@ -152,12 +155,14 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.CalendarStandard;
 		viewPanel.add(filterList, gbc_filterList);
 		
 		addButtonPanel();
+		this.repaint();
+		this.revalidate();
 	}
 	
 	/**
 	 * Method editingMode.
 	 */
-	public void editingFilterMode(){
+	public void editFilterMode(){
 		
 		editPanel = new JPanel();
 		editPanel.setBackground(Color.WHITE);
@@ -264,6 +269,8 @@ import edu.wpi.cs.wpisuitetng.modules.calendar.CalendarStandard;
 		editPanel.add(catBttnPanel, gbc_catBttnPanel);
 		
 		addButtonPanel2();
+		this.repaint();
+		this.revalidate();
 	}
 
 private void addButtonPanel(){
@@ -284,6 +291,8 @@ private void addButtonPanel(){
 			btnCancel.setText("New Filter");
 		}
 
+		btnNewFilter.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		
 		//Add Edit button
 		try {
 			final Image img = ImageIO.read(getClass().getResource("Edit_Icon.png"));
@@ -340,5 +349,14 @@ private void addButtonPanel(){
 		buttonPanel2.add(btnCancelFilter, BorderLayout.EAST);
 		// Set the horizontal gap
 		editPanel.add(buttonPanel2, gbc_btnPanel2);
+	}
+	
+	public void addListeners(){
+		btnNewFilter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				editFilterMode();
+			}
+		});
 	}
 }
