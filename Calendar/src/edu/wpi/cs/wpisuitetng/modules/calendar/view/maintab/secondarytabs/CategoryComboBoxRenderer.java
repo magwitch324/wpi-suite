53bfb4cdf6fe33bf3ec7b44c4255ccf73662840e
Andrew Roskuski <aroskuski@gmail.com>
2013-12-13 21:11:25 -0500

    Made category combo box renderer get the proper color

--- Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/maintab/secondarytabs/CategoryComboBoxRenderer.java
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/calendar/view/maintab/secondarytabs/CategoryComboBoxRenderer.java
@@ -22,16 +22,33 @@ public class CategoryComboBoxRenderer extends JPanel implements
 		ListCellRenderer<Category> {
 	private JPanel colorIcon;
 	private JLabel categoryName;
-
+	
+	public CategoryComboBoxRenderer(){
+		super();
+		setOpaque(true);
+		setAlignmentX(LEFT_ALIGNMENT);
+		setAlignmentY(CENTER_ALIGNMENT);
+	}
+	
 	@Override
 	public Component getListCellRendererComponent(JList<? extends Category> list,
 			Category value, int index, boolean isSelected, boolean cellHasFocus) {
 		
 		colorIcon = new JPanel();
+		colorIcon.setBackground(value.getCategoryColor());
 		categoryName = new JLabel(value.getName());
 		
 		add(colorIcon);
 		add(categoryName);
+		
+        if (isSelected) {
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
+        }
+        else {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
 		
 		
 		return this;
